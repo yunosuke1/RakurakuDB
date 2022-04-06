c99093f73a70337d3cc2995462beaea2090967d4
yunosuke <yunosuke.ide@rakus-partners.co.jp>
2022-04-06 15:13:54 +0900

    edit config

--- test_WorkStartDB.py
+++ test_WorkStartDB.py
@@ -1,4 +1,7 @@
 import pytest
+import datetime
+import datetime
+import jpholiday
 import time
 import os
 import signal
@@ -16,26 +19,29 @@ class TestWorkStartDB():
     self.driver.quit()
   
   def test_workStartDB(self):
-    comment_list=["Selenium学習","Python学習","CircleCI学習","Cypress学習","負荷ツール学習"]
-    rand=random.randint(0,len(comment_list)-1)
-    self.driver.get("https://itb-hdb2.htdb.jp/ggutk9a/top/index")
-    self.driver.set_window_size(982, 821)
-    self.driver.find_element(By.ID, "loginId").send_keys("yunosuke.ide")
-    self.driver.find_element(By.ID, "loginPassword").send_keys("rakus@321")
-    self.driver.find_element(By.ID, "jq-loginSubmit").click()
+    holiday = jpholiday.is_holiday(datetime.date.today())
+    if not holiday:
+      comment_list=["Selenium学習","Python学習","CircleCI学習","Cypress学習","負荷ツール学習"]
+      rand=random.randint(0,len(comment_list)-1)
+      self.driver.get("https://itb-hdb2.htdb.jp/ggutk9a/top/index")
+      self.driver.set_window_size(982, 821)
+      self.driver.find_element(By.ID, "loginId").send_keys("yunosuke.ide")
+      self.driver.find_element(By.ID, "loginPassword").send_keys("rakus@321")
+      self.driver.find_element(By.ID, "jq-loginSubmit").click()
+      
+      self.driver.switch_to.frame(1)
+      time.sleep(1.0)
+      self.driver.find_element(By.CSS_SELECTOR, "#nav-dbg-100026 .fw-ovf-max").click()
+      time.sleep(1.0)
+      self.driver.find_element(By.CSS_SELECTOR, "#nav-db-100539 .fw-ovf-max").click()
+      time.sleep(1.0)
+      self.driver.find_element(By.CSS_SELECTOR, "#menuli_101055 .fw-ovf-max").click()
+      time.sleep(1.0)
+
+      self.driver.switch_to.default_content()
+      self.driver.switch_to.frame(2)
+
+      self.driver.find_element(By.ID, "field_103738").click()
+      self.driver.find_element(By.ID, "field_103738").send_keys(comment_list[rand])
+      self.driver.find_element_by_class_name("fw-btn-ok").click()
     
-    self.driver.switch_to.frame(1)
-    time.sleep(1.0)
-    self.driver.find_element(By.CSS_SELECTOR, "#nav-dbg-100026 .fw-ovf-max").click()
-    time.sleep(1.0)
-    self.driver.find_element(By.CSS_SELECTOR, "#nav-db-100539 .fw-ovf-max").click()
-    time.sleep(1.0)
-    self.driver.find_element(By.CSS_SELECTOR, "#menuli_101055 .fw-ovf-max").click()
-    time.sleep(1.0)
-
-    self.driver.switch_to.default_content()
-    self.driver.switch_to.frame(2)
-
-    self.driver.find_element(By.ID, "field_103738").click()
-    self.driver.find_element(By.ID, "field_103738").send_keys(comment_list[rand])
-    self.driver.find_element_by_class_name("fw-btn-ok").click()
