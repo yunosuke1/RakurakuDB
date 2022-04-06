f30425c1a12d4026d8d8a754c88dda1dccdad4f3
yunosuke <yunosuke.ide@rakus-partners.co.jp>
2022-04-06 12:09:16 +0900

    edit config

--- test_WorkStartDB.py
+++ test_WorkStartDB.py
@@ -16,7 +16,7 @@ class TestWorkStartDB():
     self.driver.quit()
   
   def test_workStartDB(self):
-    comment_list=["jmeter"]
+    comment_list=["Selenium学習","Python学習","CircleCI学習","Cypress学習","負荷ツール学習"]
     rand=random.randint(0,len(comment_list)-1)
     self.driver.get("https://itb-hdb2.htdb.jp/ggutk9a/top/index")
     self.driver.set_window_size(982, 821)
